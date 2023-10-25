5ddcefb8231658bc53c2eda408ac1077b21800e5
Kiimi02 <119863454+Kiimi02@users.noreply.github.com>
2023-10-25 15:14:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ yesbtn.addEventListener('click',() => {
     question.innerHTML = 'I love you too';
 });
 
-nobtn.addEventListener('click',() => {
+nobtn.addEventListener('hover',() => {
    const i =Math.floor(Math.random() * (wrapperRect.width - nobtnRect.width)) + 1;
    const j =Math.floor(Math.random() * (wrapperRect.height - nobtnRect.height)) + 1;
 
